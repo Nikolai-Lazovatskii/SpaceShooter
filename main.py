b88f458e4f1983ab4e76a2b1902cc3c96295e790
Nikolai Lazovatskii <84211815+Nikolai-Lazovatskii@users.noreply.github.com>
2023-07-25 19:07:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -35,10 +35,16 @@ bullet_img = pygame.image.load(os.path.join(img_dir, 'bullet.png')).convert()
 class Mob (pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = meteor_img
-        self.image = pygame.transform.scale(meteor_img, (40, 40))
-        self.image.set_colorkey(BLACK)
+        self.image_orig = meteor_img
+        self.image_orig = pygame.transform.scale(meteor_img, (40, 40))
+        self.image_orig.set_colorkey(BLACK)
+        self.image = self.image_orig.copy()
         self.rect = self.image.get_rect()
+        self.radius = int(self.rect.width * .85 / 2)
+        # pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
+        self.rot = 0
+        self.rot_speed = random.randrange(-8, 8)
+        self.last_update = pygame.time.get_ticks()
         self.rect.x = random.randrange(WIDTH - self.rect.width)
         self.rect.y = random.randrange(-100, -40)
         self.speedx = random.randrange(-3, 3)
@@ -47,11 +53,23 @@ class Mob (pygame.sprite.Sprite):
     def update(self):
         self.rect.x += self.speedx
         self.rect.y += self.speedy
+        self.rotate()
         if self.rect.top > HEIGHT + 10 or self.rect.right > WIDTH + 20 or self.rect.left < -25:
              self.rect.x = random.randrange(WIDTH - self.rect.width)
              self.rect.y = random.randrange(-100, -40)
              self.speedx = random.randrange(-3, 3)
              self.speedy = random.randrange(1, 8)
+
+    def rotate(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_update > 50:
+            self.last_update = now
+            self.rot = (self.rot + self.rot_speed) % 360
+            new_image = pygame.transform.rotate(self.image_orig, self.rot)
+            old_center = self.rect.center
+            self.image = new_image
+            self.rect = self.image.get_rect()
+            self.rect.center = old_center
 
 
 class Bullet (pygame.sprite.Sprite):
@@ -78,6 +96,8 @@ class Player (pygame.sprite.Sprite):
         self.image = pygame.transform.scale(player_img, (50, 50))
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
+        self.radius = 25
+        # pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
         self.speedx = 0
@@ -102,6 +122,16 @@ class Player (pygame.sprite.Sprite):
         bullets.add(bullet)
 
 
+font_name = pygame.font.match_font("helvetica")
+
+def draw_text(surf, text, size, x, y):
+    font = pygame.font.Font(font_name, size)
+    text_surface = font.render(text, True, WHITE)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x, y)
+    surf.blit(text_surface, text_rect)
+
+
 game_folder = os.path.dirname(__file__)
 img_folder = os.path.join(game_folder, "img")
 
@@ -119,6 +149,7 @@ for i in range(6):
     all_sprites.add(mob)
     mobs.add(mob)
 
+score = 0
 run = True
 
 while run:
@@ -134,13 +165,14 @@ while run:
 
     all_sprites.update()
 
-    hits = pygame.sprite.spritecollide(player, mobs, False)
+    hits = pygame.sprite.spritecollide(player, mobs, False, pygame.sprite.collide_circle)
     if hits:
         run = False
 
     hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
 
     for hit in hits:
+        score += 10
         m = Mob()
         all_sprites.add(m)
         mobs.add(m)
@@ -148,7 +180,9 @@ while run:
     screen.fill(BLACK)
     screen.blit(background, background_rect)
     all_sprites.draw(screen)
+    draw_text(screen, str(score), 24, WIDTH / 2, 12)
     pygame.display.flip()
 
 
 pygame.quit()
+
